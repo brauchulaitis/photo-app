9a9ef1d2a1f146ba323bd0603521277286261a40
Oskars Braucs <oskars.braucs@gmail.com>
2023-07-13 12:41:38 +0300

    Confirmation email in produciton

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+   :tls => true,
+   :address => "smtp.localhost:3000",
+   :port => 587,
+   :domain => "localhost:3000",
+   :authentication => :login,
+   :user_name => "[username]",
+   :password => "[password]"
+ }
